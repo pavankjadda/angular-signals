ecf6264f0ff7cbba29446db26109851fd8888a38
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-02-22 10:21:38 -0500

    initial commit

---
+++ src/employee.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Employee } from './main';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  httpClient = inject(HttpClient);
+  url =
+    'https://my-json-server.typicode.com/pavankjadda/typicode-data/employees';
+
+  getEmployees() {
+    return this.httpClient.get<Employee[]>(this.url);
+  }
+}
