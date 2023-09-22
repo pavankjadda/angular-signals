debc29ae841f7d1bc69c5953f9dee6e4f4062823
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-09-22 17:08:57 -0400

    chore(deps): updates NPM deps

--- src/employee/employee.component.ts
+++ src/employee/employee.component.ts
@@ -2,29 +2,45 @@ import {CommonModule} from '@angular/common';
 import {Component, effect, inject, signal} from '@angular/core';
 import {Employee} from '../main';
 import {EmployeeService} from './employee.service';
+import {TuiAccordionModule} from '@taiga-ui/kit';
+import {TuiExpandModule} from '@taiga-ui/core';
 
 @Component({
 	selector: 'app-employee',
 	standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiAccordionModule, TuiExpandModule],
 	template: `
-    <div style="margin:20px">
-      <h1>Signals Demo</h1>
-      <hr />
+    <tui-accordion class="container" style="width: 100%">
+      <tui-accordion-item [showArrow]="true">
+        Click to expand
+        <ng-template tuiAccordionItemContent>
+          <div style="margin:20px">
+            <h1>Signals Demo</h1>
+            <hr />
 
-      <h2>Employees</h2>
-      <div *ngFor="let employee of employees()">
-        <p>First Name: {{ employee.firstName }}</p>
-        <p>Last Name: {{ employee.lastName }}</p>
-        <p>Email: {{ employee.email }}</p>
-        <p>Phone: {{ employee.phone }}</p>
-        <p>Age: {{ employee.age }}</p>
-        <hr />
-      </div>
+            <h2>Employees</h2>
+            <div *ngFor="let employee of employees()">
+              <p>First Name: {{ employee.firstName }}</p>
+              <p>Last Name: {{ employee.lastName }}</p>
+              <p>Email: {{ employee.email }}</p>
+              <p>Phone: {{ employee.phone }}</p>
+              <p>Age: {{ employee.age }}</p>
+              <hr />
+            </div>
 
-      <br />
-    </div>
+            <br />
+          </div>
+        </ng-template>
+      </tui-accordion-item>
+    </tui-accordion>
 	`,
+  styles: [
+    `
+      .container {
+        max-width: 80rem;
+      }
+    `
+  ]
 })
 export class EmployeeComponent {
 	employeeService = inject(EmployeeService);
--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,30 @@ import {HttpClientModule} from '@angular/common/http';
 import {Component, importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {EmployeeComponent} from './employee/employee.component';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {TuiRootModule} from '@taiga-ui/core';
 
 export interface Employee {
-	firstName: string;
-	lastName: string;
-	email: string;
-	phone: string;
-	age: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  age: number;
 }
 
 @Component({
-	selector: 'app-root',
-	standalone: true,
-	template: ` <app-employee />`,
-	imports: [EmployeeComponent],
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <tui-root >
+      <app-employee style="display: flex;justify-content: center;width: auto; margin-top: 1rem"/>
+    </tui-root>
+    `,
+  imports: [EmployeeComponent, TuiRootModule],
 })
-export class App {}
+export class App {
+}
 
 bootstrapApplication(App, {
-	providers: [importProvidersFrom(HttpClientModule)],
+  providers: [importProvidersFrom( TuiRootModule, HttpClientModule),provideAnimations()],
 });
