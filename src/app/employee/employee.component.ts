abe298e192f53d8d3316feb6bfce5f7f6b74595a
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-09-25 23:42:51 -0400

    chore(deps): updates NPM deps

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { EmployeeService } from './employee.service';
-import { Store } from '@ngrx/store';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { selectAllEmployees, setEmployees } from '../state/enrollment.reducer';
-import { Employee } from '../app.component';
 
 @Component({
 	selector: 'app-employee',
@@ -14,34 +11,9 @@ import { Employee } from '../app.component';
 		<div style="margin:20px">
 			<h1>Signals Demo</h1>
 			<hr />
-
-			<!--<div>
-				<h2>Employee-1001</h2>
-				<div>
-					<p>First Name: {{ employee().firstName }}</p>
-					<p>Last Name: {{ employee().lastName }}</p>
-					<p>Email: {{ employee().email }}</p>
-					<p>Phone: {{ employee().phone }}</p>
-					<p>Age: {{ employee().age }}</p>
-					<hr />
-				</div>
-			</div>-->
-			<hr />
 			<div>
-				<h2>All Employees Signal</h2>
-				<div *ngFor="let employee of employees()">
-					<p>First Name: {{ employee.firstName }}</p>
-					<p>Last Name: {{ employee.lastName }}</p>
-					<p>Email: {{ employee.email }}</p>
-					<p>Phone: {{ employee.phone }}</p>
-					<p>Age: {{ employee.age }}</p>
-					<hr />
-				</div>
-			</div>
-
-			<div>
-				<h2>All Employees Observable</h2>
-				<div *ngFor="let employee of employees2">
+				<h2>All Employees NgNeat Query</h2>
+				<div *ngFor="let employee of result()?.data">
 					<p>First Name: {{ employee.firstName }}</p>
 					<p>Last Name: {{ employee.lastName }}</p>
 					<p>Email: {{ employee.email }}</p>
@@ -54,15 +26,8 @@ import { Employee } from '../app.component';
 	`,
 })
 export class EmployeeComponent implements OnInit {
-	store = inject(Store);
 	employeeService = inject(EmployeeService);
-	employees = toSignal(selectAllEmployees(this.store));
-	employees2: Employee[] = [];
+	result = toSignal(this.employeeService.getEmployees().result$);
 
-	ngOnInit(): void {
-		this.employeeService.getEmployees().subscribe((employees) => {
-			this.employees2 = employees;
-			this.store.dispatch(setEmployees({ employees: employees }));
-		});
-	}
+	ngOnInit(): void {}
 }
--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Employee } from '../app.component';
+import { UseQuery } from '@ngneat/query';
 
 @Injectable({
 	providedIn: 'root',
@@ -8,8 +9,15 @@ import { Employee } from '../app.component';
 export class EmployeeService {
 	httpClient = inject(HttpClient);
 	url = 'https://my-json-server.typicode.com/pavankjadda/typicode-data/employees';
+	private useQuery = inject(UseQuery);
 
 	getEmployees() {
-		return this.httpClient.get<Employee[]>(this.url);
+		return this.useQuery(
+			['employees'],
+			() => {
+				return this.httpClient.get<Employee[]>(this.url);
+			},
+			{ staleTime: 1000 * 60 * 60 }
+		);
 	}
 }
